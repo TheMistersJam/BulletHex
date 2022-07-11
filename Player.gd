1c6b41b7cb55944b2b35079c1667da679c82150c
Mangmasta <mangmasta@gmail.com>
2022-07-11 04:21:10 -0700

    Added aiming cursor

--- Player.gd
+++ Player.gd
@@ -7,6 +7,7 @@ extends KinematicBody
 const fall_acceleration = 75
 var speed = 400
 var motion = Vector3()
+onready var cam = get_node("/root/World/Camera")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +19,15 @@ func _process(_delta):
 	#translation += motion.normalized()*delta*speed
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+	var mouse_pos = get_viewport().get_mouse_position()
+	var lookat_pos = cam.project_ray_origin(mouse_pos)
+	lookat_pos += cam.project_ray_normal(mouse_pos) * (cam.global_transform.origin.y - global_transform.origin.y )
+	#lookat_pos = get_world().direct_space_state.intersect_ray
+	$an.look_at(Vector3(lookat_pos.x, global_transform.origin.y, lookat_pos.z), Vector3.UP)
+	var camrot = $an.rotation_degrees.x
+	#$an.rotation.x -= PI/4
+	#$an.look_at(lookat_pos, Vector3.UP)
 
 func _physics_process(delta):
 	var old_y = motion.y
